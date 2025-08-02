3df7562b41206f0d81309f079c07474272bcb5f6
mthxdev <mathieu.dev.pro@proton.me>
2025-08-02 23:26:27 +0200

    Ajout de la classe Calculator avec les opérations de base

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+class Calculatrice:
+    def __init__(self):
+        self.resultat = 0
 
+    def addition(self, a, b):
+        self.resultat = a + b
+        return self.resultat
+
+    def soustraction(self, a, b):
+        self.resultat = a - b
+        return self.resultat
+
+    def multiplication(self, a, b):
+        self.resultat = a * b
+        return self.resultat
+
+    def division(self, a, b):
+        if b > 0 or b < 0:
+            self.resultat = a / b
+        elif b == 0:
+            print("Erreur: Division par zéro")
+        return self.resultat
